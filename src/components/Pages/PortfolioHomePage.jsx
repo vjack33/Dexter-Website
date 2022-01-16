670a40b2ab9f4db0f667736bf79da68ff0700dc4
Vaibhav Chopade <vaibhavchopade33@gmail.com>
2022-01-16 21:24:37 +0530

    Footer Icons updated Portfolio placeholder added

--- src/components/Pages/PortfolioHomePage.jsx
+++ src/components/Pages/PortfolioHomePage.jsx
@@ -1,5 +1,8 @@
 import ProgressBar from "../Cards/ProgressBar";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt, faPhone, faMailBulk } from '@fortawesome/free-solid-svg-icons'
+
 export default function PortfolioHomePage() {
     return (
         <>
@@ -14,12 +17,12 @@ export default function PortfolioHomePage() {
                     <img className={"heroBackground"} src={"/HeroBackground.jpg"}/>
                     <div className={"heroNameDesc"}>
                         <p className={"heroName"}>I am Vaibhav Chopade</p>
-                        <p className={"heroDesc"}>Web Developer/ Android Developer/ Geek</p>
+                        <p className={"heroDesc codetext"}>Web Developer/ Android Developer/ Geek</p>
                     </div>
                 </div>
                 <div className={"aboutMeSection"}>
                     <p className={"sectionHeader"}>About me</p>
-                    <p className={"aboutMeText"}>
+                    <p className={"aboutMeText pb-40"}>
                         My name is Vaibhav Chopade. I am curiosity-driven Developer with 3 years of experience writing
                         code for high-paced business. Eager
                         to modernizing world-class products and delivering incredible user experience. In previous role
@@ -38,14 +41,14 @@ export default function PortfolioHomePage() {
                             <div className={"skillCenterSide"}/>
                             <div className={"skillRightSide"}>
                                 <p>Skills</p>
+                                <p>Android Development</p>
+                                <ProgressBar hideProgressText={true} progress={70}/>
+                                <p>Web Development</p>
+                                <ProgressBar hideProgressText={true} progress={60}/>
                                 <p>HTML CSS</p>
-                                <ProgressBar hideProgressText={true} progress={30}/>
+                                <ProgressBar hideProgressText={true} progress={60}/>
                                 <p>HTML CSS</p>
-                                <ProgressBar hideProgressText={true} progress={30}/>
-                                <p>HTML CSS</p>
-                                <ProgressBar hideProgressText={true} progress={30}/>
-                                <p>HTML CSS</p>
-                                <ProgressBar hideProgressText={true} progress={30}/>
+                                <ProgressBar hideProgressText={true} progress={60}/>
                                 <p/>
                             </div>
                         </div>
@@ -53,24 +56,27 @@ export default function PortfolioHomePage() {
                 </div>
                 <div>
                     <p className={"sectionHeader"}>Portfolio</p>
+                    <div className={"card lighterBg p-20 m-20"}>
+                        <h1 >Somethingis cooking...</h1>
+                    </div>
                 </div>
                 <div>
                     <p className={"sectionHeader"}>Contact</p>
                     <div className={"contactSection "}>
                         <div className={"card footerItem  lighterBg pt-20"}>
-                            <img src={"/logo192.png"}/>
+                            <FontAwesomeIcon icon={faMapMarkerAlt}size={"2x"} />
                             <p>Address</p>
-                            <p>Mumbai - 400701</p>
+                            <p>Mumbai - 400701, Earth C-147</p>
                         </div>
                         <div className={"card footerItem  lighterBg pt-20 "}>
-                            <img src={"/logo192.png"}/>
-                            <p>Address</p>
-                            <p>Mumbai - 400701</p>
+                            <FontAwesomeIcon icon={faPhone} size={"2x"} />
+                            <p>Phone</p>
+                            <p>+91 9730592846</p>
                         </div>
-                        <div className={"card  footerItem  lighterBg pt-20"}>
-                            <img src={"/logo192.png"}/>
-                            <p>Address</p>
-                            <p>Mumbai - 400701</p>
+                        <div className={"card footerItem  lighterBg pt-20"}>
+                            <FontAwesomeIcon icon={faMailBulk}  size={"2x"}/>
+                            <p>Email</p>
+                            <p>vaibhavchopade33@gmail.com</p>
                         </div>
                     </div>
                 </div>
